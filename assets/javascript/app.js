14fe68cbfc711c80e89b38c617bded81f029b27f
dcejas <dcejas430@gmail.com>
2018-02-24 20:27:34 -0500

    Initial html layout; and some JS Pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,20 @@
+// 1st page - START BUTTON (initiates game, begins timer)
+    // Create buton & function to start the game (large start button)
+
+
+// 2nd page - QUESTIONNAIRE
+    // Create a var for time clock
+        // Include countdown timer
+    // Create var'S for the answers
+        // Need to link each var to the divs in html
+    // Create FORM
+        // Limit to no more than 1 answer per question
+
+
+// 3rd page - RESULTS 
+    // Calculate answers and show tallies at the end
+        // Results page takes over questionnaire page
+
+// DONT FORGET:
+    // THE READ-ME FILE!!! Resources available at Class Repo
+    // ADD PIECE TO MY PORTFOLIO
