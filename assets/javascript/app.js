3074ff8f3642bfed4cafca9b1e95fcc75a85adda
dcejas <dcejas430@gmail.com>
2018-03-12 00:03:56 -0400

    Some final cleanup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -55,7 +55,7 @@ var questions = [{
 var game = {
   correct: 0,
   incorrect: 0,
-  counter: 120,
+  counter: 60,
 
   countdown: function(){
     game.counter--;
@@ -70,7 +70,7 @@ var game = {
 
   start: function() {
     timer = setInterval(game.countdown, 1000);
-    $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">120</span> Seconds</h2></br>');
+    $('#subwrapper').prepend('<h2>Time Remaining: <span id="counter-number">60</span> Seconds</h2></br>');
     $('#start').remove();
 
     for (var i = 0; i < questions.length; i++) {
